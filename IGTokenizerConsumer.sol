b6ae847383cacd1b3201a59a7a8cffe541153579
Faysal Badaoui <faysalbadaoui02@gmail.com>
2024-03-27 21:22:41 +0100

    smart contract comlete

--- IGTokenizerConsumer.sol
+++ IGTokenizerConsumer.sol
@@ -4,36 +4,29 @@ pragma solidity ^0.8.7;
 import {Chainlink, ChainlinkClient} from "@chainlink/contracts@0.8.0/src/v0.8/ChainlinkClient.sol";
 import {ConfirmedOwner} from "@chainlink/contracts@0.8.0/src/v0.8/shared/access/ConfirmedOwner.sol";
 import {LinkTokenInterface} from "@chainlink/contracts@0.8.0/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
-
-/**
- * Request testnet LINK and ETH here: https://faucets.chain.link/
- * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
- */
-
-/**
- * THIS IS AN EXAMPLE CONTRACT WHICH USES HARDCODED VALUES FOR CLARITY.
- * THIS EXAMPLE USES UN-AUDITED CODE.
- * DO NOT USE THIS CODE IN PRODUCTION.
- */
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract IGTokenizerConsumer is ChainlinkClient, ConfirmedOwner {
     using Chainlink for Chainlink.Request;
+    using Strings for uint96;
 
     uint256 public volume;
     bytes32 private jobId;
     uint256 private fee;
 
-    event RequestVolume(bytes32 indexed requestId, uint256 volume);
+    struct IGTokenization {
+        string postId;
+        address requester;
+        string postTokenId;
+        bool isPending;
+        bool isVerified;
+    }
 
-    /**
-     * @notice Initialize the link token and target oracle
-     *
-     * Sepolia Testnet details:
-     * Link Token: 0x779877A7B0D9E8603169DdbD7836e478b4624789
-     * Oracle: 0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD (Chainlink DevRel)
-     * jobId: ca98366cc7314957b8c012c72f05aeeb
-     *
-     */
+    mapping(bytes32 => IGTokenization) public requests;
+
+    event TokenizationRequest(bytes32 indexed requestId, string postId, string hashId);
+    event InstagramPostVerification(bytes32 indexed requestId, string postId, bool isVerified);
+
     constructor() ConfirmedOwner(msg.sender) {
         setChainlinkToken(0x779877A7B0D9E8603169DdbD7836e478b4624789);
         setChainlinkOracle(0xDE6fe1bC4Dda932e0a5b557DdB10bbe5878ee752);
@@ -45,28 +38,24 @@ contract IGTokenizerConsumer is ChainlinkClient, ConfirmedOwner {
         Chainlink.Request memory req = buildChainlinkRequest(
             jobId,
             address(this),
-            this.fulfill.selector
+            this.processVerification.selector
         );
         req.add("postId", postIg);
         req.add("hashToVerify", hashVerify);
 
-        return sendChainlinkRequest(req, fee);
+        requestId = sendChainlinkRequest(req, fee);
+        requests[requestId] = IGTokenization(postIg, msg.sender, hashVerify, true, false);
+        emit TokenizationRequest(requestId, postIg, hashVerify);
+        return requestId;
     }
 
-    /**
-     * Receive the response in the form of uint256
-     */
-    function fulfill(
-        bytes32 _requestId,
-        uint256 _volume
-    ) public recordChainlinkFulfillment(_requestId) {
-        emit RequestVolume(_requestId, _volume);
-        volume = _volume;
+    function processVerification(bytes32 requestId, bool valid) virtual public recordChainlinkFulfillment(requestId) {
+        require(requests[requestId].isPending, "Oracle request already resolved");
+        requests[requestId].isPending = false;
+        requests[requestId].isVerified = valid;
+        emit InstagramPostVerification(requestId, requests[requestId].postId, valid);
     }
 
-    /**
-     * Allow withdraw of Link tokens from the contract
-     */
     function withdrawLink() public onlyOwner {
         LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
         require(
